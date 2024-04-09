018a53f7d685268461b78c936714865ea0773a8b
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-04-09 19:55:53 +0200

    commit

---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {HeaderComponent} from './components/header/header.component';
+import {RouterModule} from '@angular/router';
+import {SharedModule} from "../shared/shared.module";
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    SharedModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class CoreModule {
+}
---
+++ src/app/modules/shared/material/material.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule {
+}

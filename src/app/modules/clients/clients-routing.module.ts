a0be4c1ff5d96f9a7db32e6510eb7abd5564cd58
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-04-22 07:19:56 +0200

    auth guard fun

--- src/app/modules/clients/clients-routing.module.ts
+++ src/app/modules/clients/clients-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientsComponent } from "./clients.component";
 import { ClientComponent } from "./components/client/client.component";
 import { ClientFormComponent } from "./components/client-form/client-form.component";
-import { AuthActivateGuard } from "../core/guards/auth-activate.guard";
+//import { AuthActivateGuard } from "../core/guards/auth-activate.guard";
+import { authGuardActivate } from "../core/guards/auth-activate.function.guard";
 
 const routes: Routes = [
   {
     path: '', component: ClientsComponent,
-    canActivate: [AuthActivateGuard]
+    //canActivate: [AuthActivateGuard] użycie klasy
+    canActivate:[authGuardActivate] //użycie funkcji
   },
   {
     path: 'dodaj', component: ClientFormComponent

68ef581bb772b78f5af2632f62660a0578a740ce
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-04-25 06:45:43 +0200

    [fullstack][FE] activation account

--- projekt-wspolny-fe/src/app/modules/auth/components/account-activation/account-activation.component.ts
+++ projekt-wspolny-fe/src/app/modules/auth/components/account-activation/account-activation.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthService } from "../../../core/services/auth.service";
+import { switchMap } from "rxjs";
+import { AuthResponse } from "../../../core/models/auth.model";
+import { NotifierService } from "angular-notifier";
 
 @Component({
   selector: 'app-account-activation',
   templateUrl: './account-activation.component.html',
   styleUrls: ['./account-activation.component.scss']
 })
-export class AccountActivationComponent implements OnInit{
-
-  constructor(private route: ActivatedRoute) {
+export class AccountActivationComponent implements OnInit {
+  errorMessage: null | string = null;
+  constructor(private route: ActivatedRoute,
+              private authService: AuthService,
+              private notifierService: NotifierService,
+              private router: Router
+  ) {
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe({
-      next: params => {
-        console.log(params.get("uid"));
-      }
-    });
+    this.route.paramMap
+      .pipe(
+        switchMap((params) =>
+          this.authService.activateAccount(params.get('uid') as string)
+        )
+      )
+      .subscribe({
+        next: (response) => {
+          //
+          this.router.navigate(['/logowanie']);
+          this.notifierService.notify('success', response.message);
+        },
+        error: (err) => {
+          this.errorMessage = err;
+        },
+      });
   }
+
 }

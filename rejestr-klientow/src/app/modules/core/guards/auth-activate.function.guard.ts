3a7d907e7d0000d5b836ea5f25d8e1a1b68ecd2b
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-04-29 07:08:25 +0200

    [BE] clean sandbox

--- rejestr-klientow/src/app/modules/core/guards/auth-activate.function.guard.ts
+++
@@ -1,8 +0,0 @@
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
-import { inject } from "@angular/core";
-import { AuthService } from "../services/auth.service";
-
-export const authGuardActivate: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const isLoggedIn = inject(AuthService).isLoggedIn();
-  return isLoggedIn ? isLoggedIn : inject(Router).createUrlTree(['/logowanie']);
-}
--- todo/src/app/store/app.reducer.ts
+++
@@ -1,5 +0,0 @@
-import { TodoListState } from "../todo-list/store/todo-list.reducer";
-
-export interface AppState {
-  todos: TodoListState
-}

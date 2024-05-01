ccacc945a6cc0c9622085d772571e4f6f1adb4d0
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-05-01 16:10:43 +0200

    clean sandbox

--- rejestr-klientow/src/app/modules/core/guards/auth-load.guard.ts
+++
@@ -1,8 +0,0 @@
-import { CanLoadFn, Route, Router, UrlSegment } from "@angular/router";
-import { inject } from "@angular/core";
-import { AuthService } from "../services/auth.service";
-
-export const authGuardLoad: CanLoadFn = (route: Route, segments: UrlSegment[]) => {
-  const isLoggedIn = inject(AuthService).isLoggedIn();
-  return isLoggedIn ? isLoggedIn : inject(Router).createUrlTree(['/logowanie']);
-}

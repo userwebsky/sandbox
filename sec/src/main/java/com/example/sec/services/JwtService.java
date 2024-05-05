66f9b496fd48a644b5012385523eab54ea17d43f
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-05-05 17:15:34 +0200

    [BE] jwt service

---
+++ sec/src/main/java/com/example/sec/services/JwtService.java
@@ -0,0 +1,74 @@
+package com.example.sec.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+  public static final String SECRET = "D6E8A9791B6F0CE7E578AE04D3680713FDE7CFC38A09DBDEF5C0A7816DB1B2CA";
+
+
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+  //wyodrębnia datę z tokenu
+  public Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
+  //wyodrębnia dane z tokenu jwt
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts
+      .parser()
+      .setSigningKey(getSignKey())
+      .build()
+      .parseClaimsJws(token)
+      .getBody();
+  }
+
+  private Boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  public Boolean validateToken(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
+
+
+  public String generateToken(String userName){
+    Map<String,Object> claims=new HashMap<>();
+    return createToken(claims,userName);
+  }
+
+  private String createToken(Map<String, Object> claims, String userName) {
+    return Jwts.builder()
+      .claims(claims)
+      .subject(userName)
+      .issuedAt(new Date(System.currentTimeMillis()))
+      .expiration(new Date(System.currentTimeMillis()+1000*60*30))
+      .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+  }
+
+  private Key getSignKey() {
+    byte[] keyBytes= Decoders.BASE64.decode(SECRET);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+}

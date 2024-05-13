a3d4431f7cd2d430b0db8ace592311a70765c73d
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-05-13 08:51:05 +0200

    [BE] add repository, mediator and service

--- family-example-app/src/main/java/com/example/familyexampleapp/mediator/FamilyMediator.java
+++ family-example-app/src/main/java/com/example/familyexampleapp/mediator/FamilyMediator.java
@@ -1,6 +1,11 @@
 package com.example.familyexampleapp.mediator;
 
+import com.example.familyexampleapp.model.FamilyDB;
+import com.example.familyexampleapp.model.FamilyDTO;
+import com.example.familyexampleapp.model.FamilyExtendedDTO;
+import com.example.familyexampleapp.services.FamilyService;
 import com.example.familyexampleapp.translator.TranslatorFamilyDBtoFamilyDTO;
+import com.example.familyexampleapp.translator.TranslatorFamilyTDOtoFamilyDB;
 import com.example.familyexampleapp.translator.TranslatorMemberDBToMemberDTO;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +13,28 @@ import org.springframework.stereotype.Component;
 public class FamilyMediator {
   TranslatorFamilyDBtoFamilyDTO translatorFamilyDBtoFamilyDTO;
   TranslatorMemberDBToMemberDTO memberDBToMemberDTO;
+  TranslatorFamilyTDOtoFamilyDB translatorFamilyTDOtoFamilyDB;
+  FamilyService familyService;
 
   public FamilyMediator(
     TranslatorFamilyDBtoFamilyDTO translatorFamilyDBtoFamilyDTO,
-    TranslatorMemberDBToMemberDTO memberDBToMemberDTO
+    TranslatorMemberDBToMemberDTO memberDBToMemberDTO,
+    FamilyService familyService,
+    TranslatorFamilyTDOtoFamilyDB translatorFamilyTDOtoFamilyDB
   ) {
     this.translatorFamilyDBtoFamilyDTO = translatorFamilyDBtoFamilyDTO;
     this.memberDBToMemberDTO = memberDBToMemberDTO;
+    this.familyService = familyService;
+    this.translatorFamilyTDOtoFamilyDB = translatorFamilyTDOtoFamilyDB;
+  }
+
+  public void saveFamily(FamilyDTO familyDTO) {
+    FamilyDB familyDB = translatorFamilyTDOtoFamilyDB.toFamilyDB(familyDTO);
+      familyService.save(familyDB);
+  }
+
+  public void updateFamily(FamilyExtendedDTO familyDTO) {
+    FamilyDB familyDB = translatorFamilyTDOtoFamilyDB.toFamilyDB(familyDTO);
+    familyService.save(familyDB);
   }
 }
---
+++ family-example-app/src/main/java/com/example/familyexampleapp/translator/TranslatorFamilyTDOtoFamilyDB.java
@@ -0,0 +1,46 @@
+package com.example.familyexampleapp.translator;
+
+import com.example.familyexampleapp.model.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper
+public abstract class TranslatorFamilyTDOtoFamilyDB {
+
+  public FamilyDB toFamilyDB(FamilyExtendedDTO familyExtendedDTO) {
+    FamilyDB familyDB = toFamilyDB(familyExtendedDTO);
+    familyDB.getHead().setFamilyId(familyDB);
+    return familyDB;
+  }
+
+  public FamilyDB toFamilyDB(FamilyDTO familyDTO) {
+    FamilyDB familyDB = toFamilyDB(familyDTO);
+
+    return familyDB;
+  }
+
+  @Mappings({
+    @Mapping(target = "id", ignore = true),
+    @Mapping(target = "name", source = "name"),
+    @Mapping(target = "origin", source = "origin")
+  })
+  protected abstract FamilyDB toFamilyDBMap(FamilyDTO familyDTO);
+
+  @Mappings({
+    @Mapping(target = "id", ignore = true),
+    @Mapping(target = "name", source = "name"),
+    @Mapping(target = "origin", source = "origin"),
+    @Mapping(target = "head", expression = "java(toFamilyDBMap(familyExtendedDTO.getHead))")
+  })
+  protected abstract FamilyDB toFamilyDBMap(FamilyExtendedDTO familyExtendedDTO);
+
+  @Mappings({
+    @Mapping(target = "id", ignore = true),
+    @Mapping(target = "name", source = "name"),
+    @Mapping(target = "age", source = "age"),
+    @Mapping(target = "gender", source = "gender"),
+    @Mapping(target = "familyId", ignore = true),
+  })
+  protected abstract MembersDB toFamilyDBMap(MembersDTO membersDTO);
+}

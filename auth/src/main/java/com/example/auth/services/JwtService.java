8f6af09906d993bd52850af460d1028bc37ccb03
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-05-21 07:26:02 +0200

    [BE] jwt service

---
+++ auth/src/main/java/com/example/auth/services/JwtService.java
@@ -0,0 +1,50 @@
+package com.example.auth.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtService {
+  @Value("${jwt.secret}")
+  public final String SECRET;
+
+  public final int TOKEN_EXPIRATION;
+
+  public JwtService(@Value("${jwt.secret}") String secret, @Value("${jwt.exp}") int expiration) {
+    this.SECRET = secret;
+    this.TOKEN_EXPIRATION = expiration;
+  }
+
+  public void validateToken(final String token) {
+    Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+  }
+
+  private Key getSignKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+
+  public String generateToken(String username) {
+    Map<String, Object> claims = new HashMap<>();
+    return createToken(claims, username);
+  }
+
+  private String createToken(Map<String, Object> claims, String username) {
+    return Jwts.builder()
+              .setClaims(claims)
+              .setSubject(username)
+              .setIssuedAt(new Date(System.currentTimeMillis()))
+              .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION))
+              .signWith(getSignKey(), SignatureAlgorithm.HS512)
+              .compact();
+  }
+}

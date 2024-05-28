8f53433085b417f9589b727ebb351312ab134bf8
userwebsky <33402904+userwebsky@users.noreply.github.com>
2024-05-28 09:11:08 +0200

    [BE] gateway - route validator

---
+++ gateway/src/main/java/com/example/gateway/filter/RouteValidator.java
@@ -0,0 +1,19 @@
+package com.example.gateway.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+  public static final List<String> openApiEndpoints = List.of(
+    "auth/register",
+    "auth/login",
+    "auth/validate"
+  );
+
+  //sprawdzamy czy noneMatch-em czy nie są równe bieżącemu
+  public Predicate<ServerHttpRequest> isSecure = request -> openApiEndpoints.stream().noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
